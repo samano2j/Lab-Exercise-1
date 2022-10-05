781b3e1ba4486ad0e4021f81ed0090a9dd76a614
samano2j <jemsamano@yahoo.com>
2022-10-05 13:40:38 -0600

    Added Exercise 2

---
+++ Lab Exercise 1/src/Exercise2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercise2 {
+    public static void main(String[] args) throws Exception {
+        Scanner scan;
+        int cents = 0;
+        int dollars = 0;
+
+        scan = new Scanner(System.in);
+        System.out.println("Input the cents: ");
+        cents = scan.nextInt();
+
+        dollars = cents / 100;
+        cents = cents - (dollars * 100);
+
+        System.out.println("The value is " + dollars + " dollars and " + cents + " cents.");
+
+        scan.close();
+    }
+}
